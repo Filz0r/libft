a9b507efe13328dbcb95e90aedfe2a3cd042a83e
Filipe Parreira Figueiredo <fparreir@c2r5s3.42lisboa.com>
2023-05-23 11:45:04 +0100

    updating project structure and adding printf

---
+++ src/numbers/ft_numbers.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_numbers.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 16:14:23 by fparreir          #+#    #+#             */
+/*   Updated: 2023/05/15 16:15:27 by fparreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_NUMBERS_H
+# define FT_NUMBERS_H
+
+# include "../../includes/libft.h"
+
+char	*ft_itoa(int n);
+int		ft_atoi(const char *str);
+
+# endif
