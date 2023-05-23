3e832a8ce45c6a5f70c44c02718c612469ad676c
Filipe Parreira Figueiredo <fparreir@c2r5s3.42lisboa.com>
2023-05-23 11:44:45 +0100

    updating project structure and adding printf

---
+++ includes/libft.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fparreir <fparreir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/12 15:53:22 by fparreir          #+#    #+#             */
+/*   Updated: 2023/05/23 11:38:40 by fparreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define MIN_INT -2147483648 
+# define MAX_INT 2147483647 
+# define TRUE 1
+# define FALSE 0
+
+# include "../src/char/ft_char.h" 
+# include "../src/fd/ft_fd.h" 
+# include "../src/lists/ft_lists.h" 
+# include "../src/memory/ft_memory.h" 
+# include "../src/numbers/ft_numbers.h" 
+# include "../src/string/ft_string.h" 
+# include "../src/printf/ft_printf.h"
+
+#endif
