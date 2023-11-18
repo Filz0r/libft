061f17440809988ec9138e5c4717903b34fb085b
Filz0r <filipefig26@gmail.com>
2023-11-18 16:53:53 +0000

    new non gpt powered realloc that doesn't leak memory

--- src/memory/ft_realloc.c
+++ src/memory/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 13:01:32 by fparreir          #+#    #+#             */
-/*   Updated: 2023/11/18 13:01:32 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/11/18 16:53:28 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,13 @@
 void    *ft_realloc(void *ptr, size_t size)
 {
     void    *result;
-    size_t  cp_size;
-    size_t  i;
 
-    if (size == 0)
-    {
-        free(ptr);
-        return (NULL);
-    }
     if (ptr == NULL)
         return (malloc(size));
+    if (!size)
+        return (ptr);
     result = malloc(size);
-    if (!result)
-        return (NULL);
-    cp_size = (size_t)-1;
-    if (size < (size_t)-1)
-        cp_size = size;
-    i = -1;
-    while (++i < cp_size)
-        ((char *)result)[i] = ((char *)ptr)[i];
-
+    ft_memcpy(result, ptr, size);
     free(ptr);
     return (result);
 }
