bbb66ccae000a972ae53aba4994c4edc4431b151
Filz0r <filipefig26@gmail.com>
2023-12-19 10:53:51 +0000

    added new function for cleaning string pointers

--- src/string/ft_strclean.c
+++ src/string/ft_strclean.c
@@ -1,3 +1,45 @@
-//
-// Created by filipe on 19/12/23.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strclean.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/19 10:50:56 by fparreir          #+#    #+#             */
+/*   Updated: 2023/12/19 10:50:56 by fparreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_string.h"
+
+static int	count_garbage(char *src, char c)
+{
+	int	i;
+
+	i = 0;
+	while (src && *src++)
+		if (*src == c)
+			i++;
+	return (i);
+}
+
+char	*ft_strclean(char *src, char c, size_t size)
+{
+	char	*res;
+	int		new_size;
+
+	new_size = size - count_garbage(src, c);
+	if (new_size <= 0)
+		return (NULL);
+	res = malloc(sizeof(char) * new_size + 1);
+	if (!res)
+		return (NULL);
+	while (src && *src)
+	{
+		if (*src != c)
+			*res++ = *src;
+		src++;
+	}
+	res[new_size] = '\0';
+	return (res);
+}
--- src/string/ft_string.h
+++ src/string/ft_string.h
@@ -6,7 +6,7 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 16:16:08 by fparreir          #+#    #+#             */
-/*   Updated: 2023/05/17 21:57:51 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/12/19 10:50:56 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ char	*ft_substr(char const *str, unsigned int start, size_t length);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strclean(char *src, char c, size_t size);
 char	**ft_split(char const *s, char c);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *str);
