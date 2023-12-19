4f3bacb754d67fbbe727a844f137e33872cc4ffe
Filz0r <filipefig26@gmail.com>
2023-12-19 11:34:27 +0000

    fixed read errors

--- src/string/ft_strclean.c
+++ src/string/ft_strclean.c
@@ -6,7 +6,7 @@
 /*   By: fparreir <fparreir@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 10:50:56 by fparreir          #+#    #+#             */
-/*   Updated: 2023/12/19 10:53:44 by fparreir         ###   ########.fr       */
+/*   Updated: 2023/12/19 11:33:46 by fparreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,26 @@ static int	count_garbage(char *src, char c)
 char	*ft_strclean(char *src, char c, size_t size)
 {
 	char	*res;
+	int		i;
+	int		k;
 	int		new_size;
 
 	if (!src)
 		return (NULL);
 	new_size = size - count_garbage(src, c);
+	k = 0;
+	i = 0;
 	if (new_size <= 0)
 		return (NULL);
 	res = malloc(sizeof(char) * new_size + 1);
 	if (!res)
 		return (NULL);
-	while (src && *src)
+	while (src[i])
 	{
-		if (*src != c)
-			*res++ = *src;
-		src++;
+		if (src[i] != c)
+			res[k++] = src[i];
+		i++;
 	}
-	res[new_size] = '\0';
-	free(src);
+	res[k] = '\0';
 	return (res);
 }
